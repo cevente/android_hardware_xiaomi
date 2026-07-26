c8b5d9db8146e6d038faf6a59e1d0f6f2b5c264d
cevente <cjgrenlund@gmail.com>
2026-07-26 19:57:06 +0800

    Update UdfpsSensor constructor parameters

--- sensors/v2/Sensor.h
+++ sensors/v2/Sensor.h
@@ -150,10 +150,13 @@ class UdfpsSensor : public SysfsPollingOneShotSensor {
     UdfpsSensor(int32_t sensorHandle, ISensorsEventCallback* callback)
         : SysfsPollingOneShotSensor(
                   sensorHandle, callback, "/sys/class/touch/touch_dev/fod_press_status",
-                  "/sys/class/touch/touch_dev/fod_longpress_gesture_enabled", "UDFPS Sensor",
+                  "", "UDFPS Sensor",
                   "org.lineageos.sensor.udfps",
                   static_cast<SensorType>(static_cast<int32_t>(SensorType::DEVICE_PRIVATE_BASE) +
                                           3)) {}
+
+    virtual void writeEnable(bool /* enable */) override {}
+
     virtual void fillEventData(Event& event);
     virtual bool readFd(const int fd);
 
